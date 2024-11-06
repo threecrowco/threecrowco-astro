7b4842f9a8d6b2bc69a884155dedd7917d18e295
Chris Canterbury <studio@threecrow.co>
2024-11-06 09:58:40 -0600

    published services

--- src/content/config.ts
+++ src/content/config.ts
@@ -13,6 +13,20 @@ const projectsCollection = defineCollection({
         })
 });
 
+const servicesCollection = defineCollection({
+    type: "content",
+    schema: ({ image }) =>
+        z.object({
+            title: z.string(),
+            date: z.date(),
+            layout: z.string(),
+            image: image().refine((img) => img.width >= 600, {
+                message: "Cover image must be at least 600px wide"
+            })
+        })
+});
+
 export const collections = {
-    projects: projectsCollection
+    projects: projectsCollection,
+    services: servicesCollection
 };
